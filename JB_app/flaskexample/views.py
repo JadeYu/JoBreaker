1f6959369dfdc97ad6fed1e953169bc21223330e
Jade <yzh828@berkeley.edu>
2017-06-22 13:53:35 -0700

    error page added

--- JB_app/flaskexample/views.py
+++ JB_app/flaskexample/views.py
@@ -22,16 +22,20 @@ def enter_description():
 def model_result():
     #pull 'description' from input field and store it
     description = [request.args.get('description')]
-    cleaned = raw_cleaning(description, False)
-    cleaned_grams = get_grams(description, False)
-    #cleaned_words = list(set(cleaned.iloc[0].split()))
-    prediction = text_clf.predict_proba(cleaned)
-    output = pd.DataFrame()
-    output['jobtitle'] = text_clf.classes_
-    output['probability'] = prediction[0]
-    contri_words = contributing_words(cleaned_grams, keywords)
-    output = output.sort_values(by='probability', ascending=False)
-    return render_template("output.html", title1=output.iloc[0,0],
+    if len(description[0]) < 20:
+        return render_template("error.html")
+    try:
+        cleaned = raw_cleaning(description, False)
+        cleaned_grams = get_grams(description, False)
+        #cleaned_words = list(set(cleaned.iloc[0].split()))
+        prediction = text_clf.predict_proba(cleaned)
+        output = pd.DataFrame()
+        output['jobtitle'] = text_clf.classes_
+        output['probability'] = prediction[0]
+        contri_words = contributing_words(cleaned_grams, keywords)
+        output = output.sort_values(by='probability', ascending=False)
+    
+        return render_template("output.html", title1=output.iloc[0,0],
                          title2=output.iloc[1,0],
                          title3=output.iloc[2,0],
                          title4=output.iloc[3,0],
@@ -46,4 +50,6 @@ def model_result():
                          score6=round(output.iloc[5,1],2),
                          score7=round(output.iloc[6,1],2),
                          words1=contri_words[output.iloc[0,0]],
-                         words2=contri_words[output.iloc[1,0]])  
+                         words2=contri_words[output.iloc[1,0]])
+    except:
+        return render_template("error.html")
